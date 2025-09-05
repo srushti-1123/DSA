687fbf09cd112879606e54d0728789f5475c09f9
srushti-1123 <srushtipatil1123.in>
2025-09-05 10:00:04 +0530

    Add Search in Rotated Sorted Array 2

---
+++ Arrays/java/src/SearchInRotatedSortedArray2.java
@@ -0,0 +1,17 @@
+public class SearchInRotatedSortedArray2 {
+    public static boolean search(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == target){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,0,0,1,2};
+        int target = 0;
+
+        boolean result = search(nums,0);
+        System.out.println(result);
+    }
+}

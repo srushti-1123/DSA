eef6ec265fb1005df4eeb4838d3b5084690c7165
srushti-1123 <srushtipatil1123.in>
2025-09-06 17:28:39 +0530

    Add Monotonic Array Solution

---
+++ Arrays/java/src/MonotonicArray.java
@@ -0,0 +1,20 @@
+public class MonotonicArray {
+    public static boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] > nums[i-1]){
+                decreasing = false;
+            }
+            if(nums[i] < nums[i-1]){
+                increasing = false;
+            }
+        }
+        return increasing || decreasing;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,3};
+        boolean result = isMonotonic(nums);
+        System.out.println(result);
+    }
+}
